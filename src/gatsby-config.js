3dcb5c0a713fb642b92330a61fd4672bac3cae51
Dan Marquis <daniel.marquis@uconn.edu>
2020-05-09 23:30:47 -0400

    Added primitve css and styled the header

--- src/gatsby-config.js
+++ src/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Static Site Template`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Dan Marquis`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/src/components/header.js
+++ src/src/components/header.js
@@ -1,32 +1,48 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import './header.css'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `gray`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <header className="header">
+    <div className='medium-container'>
+      <div className='flex-row'>
+        <div className='flex-large'>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+        </div>
+        <div className="flex-large nav-container">
         <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+              to="/page-2"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+              className='nav-link'
+            >
+            Page 2
         </Link>
-      </h1>
+        <Link
+              to="/page-2"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+              className='nav-link'
+            >
+            Page 3
+        </Link>
+        </div>
+      </div>
     </div>
   </header>
 )
